ace33fb3fbbdcd2e7953863423cde6d360643c69
Justin Conner <justin@remine.com>
2020-02-27 15:29:28 -0600

    Ejected

--- example/src/components/Buttons/Buttons.js
+++ example/src/components/Buttons/Buttons.js
@@ -1,81 +1,88 @@
 import React from "react";
-import { Button, H3 } from 'repaint';
+import { RepaintThemeProvider, CoreTheme } from "repaint/dist/esm/theme";
+import { Button } from "repaint/dist/esm/Inputs";
+
 import { Container, ExampleContainer } from "../../App.styled";
 import { ThemedButton } from "./Buttons.styled";
 import Text from "../Text";
 
-// const { Button, H3 } = React.lazy(() => import("repaint"));
+// const { Button } = React.lazy(() => import("repaint/dist/esm/Inputs"));
+// const DataDisplay = React.lazy(() => import("repaint/DataDisplay"));
 
-console.log({ Button, H3 });
+console.log({RepaintThemeProvider, CoreTheme}); //{ Button, H3 });
 
 export default () => {
     return (
-        <ExampleContainer>
-            <Container>
-                <React.Suspense fallback={<div>WOWOWO</div>}>
-                    <>
-                        <Button>Sup</Button>
-                        <H3>Whhaat</H3>
-                    </>
-                </React.Suspense>
-                <Text>Sizes:</Text>
-                <div>
-                    <ThemedButton size="large">Large</ThemedButton>
-                    <ThemedButton>Default</ThemedButton>
-                    <ThemedButton size="small">Small</ThemedButton>
-                </div>
-            </Container>
-            <Container>
-                <Text>Variants:</Text>
-                <div>
-                    <ThemedButton>Default</ThemedButton>
-                    <ThemedButton variant="primary">Primary</ThemedButton>
-                    <ThemedButton variant="secondary">Secondary</ThemedButton>
-                </div>
-            </Container>
-            <Container>
-                <Text>Combined:</Text>
-                <div>
-                    <ThemedButton size="large" variant="primary">
-                        Large Primary
-                    </ThemedButton>
-                    <ThemedButton variant="primary">
-                        Default Primary
-                    </ThemedButton>
-                    <ThemedButton size="small" variant="primary">
-                        Small Primary
-                    </ThemedButton>
-                    <ThemedButton size="large" variant="secondary">
-                        Large Secondary
-                    </ThemedButton>
-                    <ThemedButton variant="secondary">
-                        Default Secondary
-                    </ThemedButton>
-                    <ThemedButton size="small" variant="secondary">
-                        Small Secondary
-                    </ThemedButton>
-                    <ThemedButton>Default Default</ThemedButton>
-                </div>
-            </Container>
-            <Container>
-                <Text>Variants with boolean prop variants:</Text>
-                <div>
-                    <ThemedButton variant="primary" isActive={true}>
-                        Primary isActive
-                    </ThemedButton>
-                    <ThemedButton variant="primary" isDisabled={true}>
-                        Primary isDisabled
-                    </ThemedButton>
-                    <ThemedButton
-                        variant="primary"
-                        size="small"
-                        isActive={true}
-                        isDisabled={true}
-                        isPurple>
-                        Small Primary isActive + isDisabled + isPurple
-                    </ThemedButton>
-                </div>
-            </Container>
-        </ExampleContainer>
+        <RepaintThemeProvider theme={CoreTheme}>
+            <ExampleContainer>
+                <Container>
+                    <React.Suspense fallback={<div>WOWOWO</div>}>
+                        <>
+                            <Button>Sup</Button>
+                            {/* <DataDisplay.H3>Whhaat</DataDisplay.H3> */}
+                        </>
+                    </React.Suspense>
+                    <Text>Sizes:</Text>
+                    <div>
+                        <ThemedButton size="large">Large</ThemedButton>
+                        <ThemedButton>Default</ThemedButton>
+                        <ThemedButton size="small">Small</ThemedButton>
+                    </div>
+                </Container>
+                <Container>
+                    <Text>Variants:</Text>
+                    <div>
+                        <ThemedButton>Default</ThemedButton>
+                        <ThemedButton variant="primary">Primary</ThemedButton>
+                        <ThemedButton variant="secondary">
+                            Secondary
+                        </ThemedButton>
+                    </div>
+                </Container>
+                <Container>
+                    <Text>Combined:</Text>
+                    <div>
+                        <ThemedButton size="large" variant="primary">
+                            Large Primary
+                        </ThemedButton>
+                        <ThemedButton variant="primary">
+                            Default Primary
+                        </ThemedButton>
+                        <ThemedButton size="small" variant="primary">
+                            Small Primary
+                        </ThemedButton>
+                        <ThemedButton size="large" variant="secondary">
+                            Large Secondary
+                        </ThemedButton>
+                        <ThemedButton variant="secondary">
+                            Default Secondary
+                        </ThemedButton>
+                        <ThemedButton size="small" variant="secondary">
+                            Small Secondary
+                        </ThemedButton>
+                        <ThemedButton>Default Default</ThemedButton>
+                    </div>
+                </Container>
+                <Container>
+                    <Text>Variants with boolean prop variants:</Text>
+                    <div>
+                        <ThemedButton variant="primary" isActive={true}>
+                            Primary isActive
+                        </ThemedButton>
+                        <ThemedButton variant="primary" isDisabled={true}>
+                            Primary isDisabled
+                        </ThemedButton>
+                        <ThemedButton
+                            variant="primary"
+                            size="small"
+                            isActive={true}
+                            isDisabled={true}
+                            isPurple>
+                            Small Primary isActive + isDisabled + isPurple
+                        </ThemedButton>
+                    </div>
+                </Container>
+            </ExampleContainer>
+        </RepaintThemeProvider>
     );
 };
